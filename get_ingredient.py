7196473782dae0765e3420b3670bc2d6fd450992
Jean.Milou.Pierre <miloujpierre@gmail.com>
2018-09-23 11:52:09 -0400

    Ajout du dictionnaire pour classer les lignes de texte

--- get_ingredient.py
+++ get_ingredient.py
@@ -2,9 +2,28 @@ import re
 from typing import Any
 
 
+def classer_text(file):
+    """ Classer les textes dans un dictionnaire en fonction du nombre de mot"""
+    dico = dict()
+    with open(file,encoding='utf8') as fichier:
+        m = 0
+        for ligne in fichier:
+           m += 1
+           if ligne[0].isnumeric():
+                ma_list = ligne.split()
+                n = len(ma_list)
+                if n not in dico.keys():
+                    dico[n]= []
+                    dico[n].append(ma_list)
+                else:
+                    dico[n].append(ma_list)
+    #print(m)
+    print(dico)
+classer_text("ingredients.txt")
+
 def affiche_longeur_3(text):
     """
-    Permet d'afficher les textes commancant par un chiffre et dont le nombre de trois est mots
+    Permet d'afficher les textes commancant par un chiffre et dont le nombre de mot est trois
     :type text: string
 
     """
@@ -24,4 +43,17 @@ def affiche_longeur_3(text):
         pass
 
 
-affiche_longeur_3("2 gousses d'ail")
+def affiche_longeur_2(text):
+    """
+    Permet d'afficher les textes commancant par un chiffre et dont le nombre de mots est trois
+    :type text: string
+
+    """
+
+    if text[0].isnumeric():
+        split_text = text.split()  # type: string
+        if len(split_text) == 2:
+            print(split_text[0] + ', ' + split_text[1])
+    else:
+
+        pass
---
+++ my_main.py
@@ -0,0 +1,7 @@
+import get_ingredient
+
+if __name__ == "__main__":
+
+    #affiche_longeur_3("2 gousses d'ail")
+    print([list()].append([1,2,3]))
+    classer_text("ingredients.txt")

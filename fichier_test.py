5fcbf5e67ad29bc307c30f545d71e22813dd3da0
Jean.Milou.Pierre <miloujpierre@gmail.com>
2018-09-25 01:05:14 -0400

    gestion des ligne débutant  par number qui a 4 mot

--- fichier_test.py
+++ fichier_test.py
@@ -36,14 +36,14 @@ def classer_text(file):
 #classer_text3("ingredients.txt")
 
 
-def affiche_longeur3(text3, nb):
+def affiche_longeur3(text, nb):
     """
     Permet d'afficher les textes commancant par un chiffre et dont le nombre de mot est trois
     :type text: string
     :type nb: nombre d'élément dans la liste
     """
     dico={}
-    dico = classer_text3(text)
+    dico = classer_text(text)
 
     dico =dico[nb]
     m = 0
@@ -76,76 +76,166 @@ def affiche_longeur3(text3, nb):
        else:
            print(to_string+ "  Résultat OUI: "  + split_text[2].replace("d'","").replace("d’","") + ', ' + split_text[0] + ' ' + split_text[1])
     #print(m)
+def affiche_longeur_2(text):
+    """
+    Permet d'afficher les textes commancant par un chiffre et dont le nombre de mots est trois
+    :type text: string
+
+    """
+    for ligne in text:
+        #split_text = ligne.split()  # type: string
+        print(ligne[0] + ', ' + (ligne[1]).replace("d'",""))
+
+def affiche_longeur_4(text):
+    for ligne in text:
+        my_string = ligne[0] + " " + ligne[1] + " " + ligne[2] + " " +ligne[3]
+
+        if re.search(r"purée$|échalotes|oignon", my_string) :
+            print(my_string + " Ingrédients: " + ligne[1] + ', ' + "Quantité: " + ligne[0] )
+        elif re.search(r"\d ((\w+)|(c.à.c)) de (\w+)", my_string):
+            if "lime" in ligne:
+                 print(my_string + " Ingrédients: " + ligne[1]+" "+ ligne[2] + " "+ ligne[3] + ', ' + "Quantité: " + ligne[0] )
+            else:
+                 print(my_string + " Ingrédients: "  + ligne[3] + ', ' + "Quantité: " + ligne[0] + " " +ligne[1])
+        elif re.search(r"hachée$|française|d’ail,|saucisses", my_string):
+            print(my_string + " Ingrédients: " + ligne[1] + " " +(ligne[2]).replace("d’","")+ ', ' + "Quantité: " + ligne[0] )
+        # elif re.search(r"", my_string):
+
+        else:
+            print(my_string +" Ingrédients: "+ligne[2].replace("d'","").replace("d’","") + " " +ligne[3] + ', '+ "Quantité: " + ligne[0] + " " +ligne[1])
 
 
+
+
+def affiche100_len_1(len1):
+    """ Cette fonction affiche les ligne qui ont um seul mot et ne commanssant pas par un nombre"""
+    print("Ingrédients: " + len1[0] + ", " + "Quantité:     ")
+
+
+def affiche100_len_2(len2):
+    """ Cette fonction affiche les ligne qui ont deux mots et ne commanssant pas par un nombre"""
+    if "Feuilles" in len2:
+        print("Ingrédients: " + (len2[1]).replace("d'", "") + "  " + "Quantité: " + len2[0])
+    else:
+        print("Ingrédients: " + (len2[0]).replace("des", "") + " " + len2[1] + ", " + "Quantité:     ")
+
+def affiche100_len_3(len3):
+    """ Cette fonction affiche les ligne qui ont trois mots et ne commanssant pas par un nombre"""
+    if "Préparation" in len3 or "Finition" in len3:
+        print(len3[0] + " " + len3[1] + " " + len3[2] + " Ingrédients: " + ",  " + "Quantité:     ")
+    elif "pour" in len3 or "du" in len3:
+        print(len3[0] + " " + len3[1] + " " + len3[2] + " Ingrédients: " + len3[0] + ", " + "Quantité:     ")
+    elif "goût" in len3:
+        print(len3[0] + " " + len3[1] + " " + len3[2] + " Ingrédients: " + len3[0] + ", " + "Quantité: " + len3[
+            1] + " " + len3[2])
+    elif "quelques" in len3:
+        print(len3[0] + " " + len3[1] + " " + len3[2] + " Ingrédients: " + len3[2] + ", " + "Quantité: " + len3[
+            0] + " " + len3[1])
+    elif "Quelques" in len3:
+        print(len3[0] + " " + len3[1] + " " + len3[2] + " Ingrédients: " + len3[1] + ", " + "Quantité: " + len3[0])
+    elif "Zeste" in len3:
+        print(len3[0] + " " + len3[1] + " " + len3[2] + " Ingrédients: " + len3[2] + ", " + "Quantité: " + (
+        len3[1]).replace("d'", ""))
+    else:
+        print(len3[0] + " " + len3[1] + " " + len3[2] + " Ingrédients: " + len3[0] + " " + len3[1] + " " + len3[
+            2] + ", " + "Quantité:     ")
+
+
+def affiche100_len_4(len4):
+    """ Cette fonction affiche les ligne qui ont um seul mot et ne commanssant pas par un nombre"""
+    if "Quelques" in len4:
+        print(len4[0] + " " + len4[1] + " " + len4[2] + " " + len4[3] +
+              " Ingrédients: " + len4[3] + ", " + "Quantité: " + len4[0] + " " + len4[1])
+    elif "Préparation" in len4:
+        print(len4[0] + " " + len4[1] + " " + len4[2] + " " + len4[3] + " Ingrédients: " + ",  " + "Quantité:     ")
+    elif "pincée" in len4:
+        print(len4[0] + " " + len4[1] + " " + len4[2] + " " + len4[3] + " Ingrédients: " + len4[
+            3] + ",  " + "Quantité: " + len4[0] + " " + len4[1])
+    elif "sel" in len4:
+        print(len4[0] + " " + len4[1] + " " + len4[2] + " " + len4[3] + " Ingrédients: " + len4[1] + " " + len4[
+            3] + ",  " + "Quantité: ")
+    elif "citron" in len4:
+        print(len4[0] + " " + len4[1] + " " + len4[2] + " " + len4[3] + " Ingrédients: " + len4[
+            3] + ",  " + "Quantité: " + (len4[2]).replace("d'", ""))
+    elif "demi-lime" in len4:
+        chaine_lime = (len4[2] + ' ' + len4[3]).split("-")
+        print(len4[0] + " " + len4[1] + " " + len4[2] + " " + len4[3] + " Ingrédients: " + chaine_lime[
+            1] + ",  " + "Quantité: " + (chaine_lime[0]).replace("d’", ""))
+    elif "quinzaine" in len4:
+        print(len4[0] + " " + len4[1] + " " + len4[2] + " " + len4[3] + " Ingrédients: " + (len4[2]).replace("d’",
+                                                                                                                  "") + ",  " + "Quantité: " +
+              len4[0] + " " + len4[1])
+
+    else:
+        print(len4[0] + " " + len4[1] + " " + len4[2] + " " + len4[3] +
+              " Ingrédients: " + len4[0] + " " + len4[1] + " " + len4[2] + " " + len4[3] + ", " + "Quantité:     ")
+
+def affiche100_len_5(len5):
+    """ Cette fonction affiche les ligne qui ont 5 mots et ne commanssant pas par un nombre"""
+    if "Préparation" in len5:
+        print(len5[0] + " " + len5[1] + " " + len5[2] + " " + len5[3] + " " + len5[
+            4] + " Ingrédients: " + ",  " + "Quantité:     ")
+    elif "Feuille" in len5:
+        print(len5[0] + " " + len5[1] + " " + len5[2] + " " + len5[3] + " " + len5[4] + " Ingrédients: " + len5[
+            2] + ",  " +
+              "Quantité: " + len5[0] + " " + len5[3] + " " + len5[4])
+    else:
+        print(len5[0] + " " + len5[1] + " " + len5[2] + " " + len5[3] + " " + len5[4] + " Ingrédients: " +
+              len5[0] + " " + len5[1] + " " + len5[2] + ",  " +
+              "Quantité: " + len5[3] + " " + len5[4])
+
+def affiche100_len_6(len6):
+    my_string = len6[0] + " " + len6[1] + " " + len6[2] + " " + len6[3] + " " + len6[4] + " " + len6[5]
+    if "une" in len6:
+        print( my_string + " Ingrédients: " +
+              len6[0] + " " + len6[1] + " " + len6[2] + ",  " +  "Quantité: " + len6[4] + " " + len6[5])
+    else:
+        print(my_string+ " Ingrédients: " + ",  " +
+        "Quantité: " )
+
+def affiche100_len_7(len7):
+    my_string = len7[0] + " " + len7[1] + " " + len7[2] + " " + len7[3] + " " + len7[4] + " " + len7[5]+ " " + len7[6]
+    print(my_string)
+    if "moulin" in len7 :
+       print( my_string + " Ingrédiant: "+len7[0] + " " + len7[1] + " " + len7[2] + " " + len7[3] + " " + len7[4]
+              + ",  " + "Quantité: " + len7[5]+ " " + len7[6])
+    else:
+        print(my_string + " Ingrédiant: "+ len7[1] + " " + len7[2] + " " + len7[3] + " " + len7[4]+ " " +len7[5]
+              + ",  " + "Quantité: "  + len7[0])
 def affiche_longeur100(text100):
     ma_liste = classer_text(text100)[100]
     for ligne in  ma_liste:
-      if len(ligne) == 5:
         lmot = len(ligne)
         if lmot == 1:
-            print("Ingrédients: " +ligne[0] + ", "+ "Quantité:     ")
-
+            affiche100_len_1(ligne)
         elif lmot == 2:
-            if "Feuilles" in ligne:
-                print("Ingrédients: " + (ligne[1]).replace("d'", "")+ "  " +"Quantité: " +ligne[0] )
-            else:
-                 print("Ingrédients: " + (ligne[0]).replace("des", "") +" "+ ligne[1] +", " + "Quantité:     ")
+            affiche100_len_2(ligne)
         elif lmot == 3:
-            if "Préparation" in ligne or "Finition" in ligne:
-                print(ligne[0] + " " + ligne[1] + " " + ligne[2] +" Ingrédients: " + ",  " + "Quantité:     ")
-            elif "pour" in ligne or "du" in ligne  :
-                print(ligne[0] + " " + ligne[1] + " " + ligne[2] + " Ingrédients: " + ligne[0] + ", " + "Quantité:     ")
-            elif "goût" in ligne:
-                print(ligne[0] + " " + ligne[1] + " " + ligne[2] + " Ingrédients: " + ligne[0] + ", " + "Quantité: " +ligne[1] + " " + ligne[2])
-            elif "quelques" in ligne:
-                print(ligne[0] + " " + ligne[1] + " " + ligne[2] + " Ingrédients: " + ligne[2] + ", " + "Quantité: " + ligne[0] + " " + ligne[1])
-            elif "Quelques" in ligne:
-                print(ligne[0] + " " + ligne[1] + " " + ligne[2] + " Ingrédients: " + ligne[1] + ", " + "Quantité: " + ligne[0] )
-            elif "Zeste" in ligne:
-                print(ligne[0] + " " + ligne[1] + " " + ligne[2] + " Ingrédients: " + ligne[2] + ", " + "Quantité: " + (ligne[1]).replace("d'","") )
-            else:
-                print(ligne[0]+" " +ligne[1]+" " +ligne[2] +" Ingrédients: " + ligne[0]+ " "+ligne[1]+ " " + ligne[2] + ", " + "Quantité:     ")
-        if len(ligne) == 4:
-          if "Quelques" in ligne:
-              print(ligne[0] + " " + ligne[1] + " " + ligne[2] + " " + ligne[3] +
-                    " Ingrédients: "  + ligne[3] + ", " + "Quantité: " + ligne[0] + " " + ligne[1])
-          elif "Préparation"  in ligne:
-                  print(ligne[0] + " " + ligne[1] + " " + ligne[2] + " " + ligne[3] +" Ingrédients: " + ",  " + "Quantité:     ")
-          elif "pincée"  in ligne:
-                  print(ligne[0] + " " + ligne[1] + " " + ligne[2] + " " + ligne[3] +" Ingrédients: " + ligne[3]+ ",  " + "Quantité: " +ligne[0] + " " + ligne[1])
-          elif "sel"  in ligne:
-             print(ligne[0] + " " + ligne[1] + " " + ligne[2] + " " + ligne[3] +" Ingrédients: " +ligne[1] + " " + ligne[3]+ ",  " + "Quantité: "  )
-          elif "citron"  in ligne:
-             print(ligne[0] + " " + ligne[1] + " " + ligne[2] + " " + ligne[3] +" Ingrédients: " +ligne[3] + ",  " + "Quantité: "+(ligne[2]).replace("d'",""))
-          elif "demi-lime"  in ligne:
-            chaine_lime= (ligne[2]+' '+ligne[3]).split("-")
-            print(ligne[0] + " " + ligne[1] + " " + ligne[2] + " " + ligne[3] +" Ingrédients: " +chaine_lime[1] + ",  " + "Quantité: "+(chaine_lime[0]).replace("d’",""))
-          elif "quinzaine"  in ligne:
-            print(ligne[0] + " " + ligne[1] + " " + ligne[2] + " " + ligne[3] +" Ingrédients: " +(ligne[2]).replace("d’", "") + ",  " + "Quantité: "+ligne[0] +" "+ligne[1])
-
-          else:
-             print(ligne[0] + " " + ligne[1] + " " + ligne[2] + " " + ligne[3] +
-                " Ingrédients: " + ligne[0] + " " + ligne[1] + " " + ligne[2] + " " + ligne[3] + ", " + "Quantité:     ")
-      if len(ligne) == 5:
-          if "Préparation"  in ligne:
-                  print(ligne[0] + " " + ligne[1] + " " + ligne[2] + " " + ligne[3] + " " + ligne[4] +" Ingrédients: " + ",  " + "Quantité:     ")
-          elif "Feuille"  in ligne:
-                  print(ligne[0] + " " + ligne[1] + " " + ligne[2] + " " + ligne[3] + " " + ligne[4] +" Ingrédients: " + ligne[2]+ ",  " +
-                        "Quantité: "+ligne[0]+ " " + ligne[3] + " " + ligne[4])
-          else:
-              print(ligne[0] + " " + ligne[1] + " " + ligne[2] + " " + ligne[3] + " " + ligne[4] + " Ingrédients: " +
-                    ligne[0] + " " +ligne[1] + " " + ligne[2]+",  " +
-                    "Quantité: " + ligne[3]+" " + ligne[4])
+            affiche100_len_3(ligne)
+        elif lmot == 4:
+            affiche100_len_4(ligne)
+        elif lmot == 5:
+            affiche100_len_5(ligne)
+        elif lmot == 6:
+            affiche100_len_6(ligne)
+        elif lmot == 7:
+            affiche100_len_7(ligne)
 
 if __name__ == "__main__":
+
     print(classer_text("ingredients.txt")[100])
-    #affiche_longeur("ingredients.txt", 3)
-    affiche_longeur100("ingredients.txt")
+    Val=classer_text("ingredients.txt")[4]
+    #print(len(Val))
+    for i in Val:
+        #if len(i)==10:
+        print (i)
+    # affiche_longeur3("ingredients.txt", 3)
+    #affiche_longeur100("ingredients.txt")
+    # affiche100_len_6(['une', 'petite', 'tasse', 'de', 'riz', 'rond'])
 
-
-
-
+    #affiche100_len_7(['trait', 'jus', 'de', 'citron', 'ou', 'de', 'lime'])
+    #affiche_longeur_2(Val)
+    affiche_longeur_4(Val)
 
 
 
